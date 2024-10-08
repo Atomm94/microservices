fa0ef53823c78a213f9d6f1277b1a3b41b50445f
Atomm94 <atom.makaryan.94@gmail.com>
2024-10-08 19:25:55 +0400

    08/10/2024 update

--- test.js
+++
@@ -1,8 +0,0 @@
-let obj = {
-    _id: '6703fff145f7f39778d7d093',
-    name: 'device60067561',
-    status: 'active',
-    lastPingTime: '10/8/2024, 1:02:02 PM'
-}
-
-console.log({ ...obj, checked: 0 })
